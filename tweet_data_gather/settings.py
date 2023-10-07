db64fe5343b152d2638ad1fbae6a466568f4ed10
etimbuk9 <etimbuk9@yahoo.co.uk>
2023-10-07 20:41:14 +0100

    secret key removed

--- tweet_data_gather/settings.py
+++ tweet_data_gather/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6pp@m7=*+cb7)1wyh8@h1%xf8ko(e4j7_e&y5i9v+&dx-=3t-&'
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
